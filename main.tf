b3f932f2dd6f6e65693a3fa216a7147c17e5d0bc
Allan Taylor <allan@startmail.com>
2021-10-03 10:11:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ resource "null_resource" "deploy_actions_runner_controller" {
   depends_on = [null_resource.setup_env]
   provisioner "local-exec" {
     command = <<-EOT
+      kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.yaml
+      sleep 180
       helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
       helm upgrade --install actions-runner-controller actions-runner-controller/actions-runner-controller \
         --namespace "${var.actions_runner_namespace}" \
