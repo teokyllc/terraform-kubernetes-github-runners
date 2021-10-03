8aa8ea189a0164d97a265b4962b34d225c90ed3d
Allan Taylor <allan@startmail.com>
2021-10-03 09:12:52 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,27 +7,34 @@ resource "null_resource" "setup_env" {
   }
 }
 
-resource "null_resource" "configure_cert_manager" {
+resource "null_resource" "deploy_actions_runner_controller" {
   depends_on = [null_resource.setup_env]
   provisioner "local-exec" {
     command = <<-EOT
-      kubectl create namespace ${var.cert_manager_namespace}
-      helm repo add jetstack https://charts.jetstack.io
-      helm repo update
-      helm install cert-manager jetstack/cert-manager \
-        --namespace ${var.cert_manager_namespace} \
-        --version ${var.cert_manager_version} \
-        --set installCRDs=true \
-        --set image.repository=${var.image_path} \
-        --set image.tag=${var.image_tag} \
-        --set image.replicaCount=${var.replicas} \
-        --set global.imagePullSecrets[0].name="artifactory-access" \
-        --set serviceAccount.name=${var.service_account_name} \
-        --set webhook.image.repository=${var.webhook_image_path} \
-        --set webhook.image.tag=${var.webhook_image_tag} \
-        --set webhook.serviceType="ClusterIP" \
-        --set cainjector.image.repository=${var.cainjector_image_path} \
-        --set cainjector.image.tag=${var.cainjector_image_tag}    
+      helm upgrade --install \
+        --namespace actions-runner-system \
+        --create-namespace \
+        --values runners-values.yaml \
+        --wait \
+        actions-runner-controller actions-runner-controller/actions-runner-controller
+    EOT
+  }
+}
+
+resource "null_resource" "deploy_actions_runner_deployment" {
+  depends_on = [null_resource.deploy_actions_runner_controller]
+  provisioner "local-exec" {
+    command = <<-EOT
+      cat <<EOF | kubectl apply -f -
+      apiVersion: actions.summerwind.dev/v1alpha1
+      kind: RunnerDeployment
+      metadata:
+        name: teokyllc-runner-deployment
+        namespace: actions-runner-system
+      spec:
+        replicas: 1
+        organization: teokyllc
+      EOF
     EOT
   }
 }
