ed3ac82d6837cb5a1e2a8ac7de5b5e9f58248fa6
Allan Taylor <allan@startmail.com>
2021-10-03 11:23:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,11 +32,44 @@ resource "null_resource" "deploy_actions_runner_deployment" {
       apiVersion: actions.summerwind.dev/v1alpha1
       kind: RunnerDeployment
       metadata:
-        name: teokyllc-runner-deployment
-        namespace: ${var.actions_runner_namespace}
+        name: actions-runner-deployment
+        namespace: actions-runner-system
       spec:
-        replicas: 1
-        organization: teokyllc
+        template:
+          metadata:
+            annotations:
+              cluster-autoscaler.kubernetes.io/safe-to-evict: "true"
+          spec:
+            organization: teokyllc
+            image: johndvs/actions-runner:v2.283.2-ubuntu-20.04
+            imagePullPolicy: IfNotPresent
+            tolerations:
+              - key: "node.kubernetes.io/unreachable"
+                operator: "Exists"
+                effect: "NoExecute"
+                tolerationSeconds: 10
+            ephemeral: true
+            dockerEnabled: true
+            dockerRegistryMirror: https://mirror.gcr.io/
+            dockerdWithinRunnerContainer: false
+            workDir: /home/runner/work
+      ---
+      apiVersion: actions.summerwind.dev/v1alpha1
+      kind: HorizontalRunnerAutoscaler
+      metadata:
+        name: runner-deployment-autoscaler
+        namespace: actions-runner-system
+      spec:
+        scaleTargetRef:
+          name: actions-runner-deployment
+        minReplicas: 1
+        maxReplicas: 10
+        metrics:
+        - type: PercentageRunnersBusy
+          scaleUpThreshold: '0.75'    # The percentage of busy runners at which the number of desired runners are re-evaluated to scale up
+          scaleDownThreshold: '0.3'   # The percentage of busy runners at which the number of desired runners are re-evaluated to scale down
+          scaleUpAdjustment: 2        # The scale up runner count added to desired count
+          scaleDownAdjustment: 1      # The scale down runner count subtracted from the desired count
       EOF
     EOT
   }
