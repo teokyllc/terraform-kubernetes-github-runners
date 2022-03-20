ebaea58df29f61ffdb612e9cabf225f03dd61ffd
Allan Taylor <allan@startmail.com>
2022-03-20 07:38:25 -0400

    wip

--- main.tf
+++ main.tf
@@ -1,50 +1,56 @@
+resource "kubernetes_namespace" "action_runner_ns" {
+  metadata {
+    name = var.actions_runner_namespace
+  }
+}
+
 resource "helm_release" "runners_controller" {
   name       = "actions-runner-controller"
   repository = "https://actions-runner-controller.github.io/actions-runner-controller"
   chart      = "actions-runner-controller"
-  namespace  = var.actions_runner_namespace
+  namespace  = kubernetes_namespace.action_runner_ns.name
   values     = [
     "${file(var.gh_actiones_values_filename)}"
   ]
 }
 
-resource "kubernetes_manifest" "actions_runner_deployment" {
-  depends_on = [helm_release.runners_controller]
-  manifest = {
-    "apiVersion" = "actions.summerwind.dev/v1alpha1"
-    "kind" = "RunnerDeployment"
-    "metadata" = {
-      "name" = "actions-runner-deployment"
-      "namespace" = "${var.actions_runner_namespace}"
-    }
-    "spec" = {
-      "template" = {
-        "metadata" = {
-          "annotations" = {
-            "cluster-autoscaler.kubernetes.io/safe-to-evict" = "true"
-          }
-        }
-        "spec" = {
-          "dockerEnabled" = "${var.docker_enabled}"
-          "dockerdWithinRunnerContainer" = "${var.docker_enabled_in_runner_container}"
-          "ephemeral" = "${var.ephemeral}"
-          "image" = "${var.container_registry}/${var.container_image}:${var.container_tag}"
-          "imagePullPolicy" = "IfNotPresent"
-          "organization" = "${var.github_org}"
-          "tolerations" = [
-            {
-              "effect" = "NoExecute"
-              "key" = "node.kubernetes.io/unreachable"
-              "operator" = "Exists"
-              "tolerationSeconds" = 10
-            },
-          ]
-          "workDir" = "/home/runner/work"
-        }
-      }
-    }
-  }
-}
+# resource "kubernetes_manifest" "actions_runner_deployment" {
+#   depends_on = [helm_release.runners_controller]
+#   manifest = {
+#     "apiVersion" = "actions.summerwind.dev/v1alpha1"
+#     "kind" = "RunnerDeployment"
+#     "metadata" = {
+#       "name" = "actions-runner-deployment"
+#       "namespace" = "${kubernetes_namespace.action_runner_ns.name}"
+#     }
+#     "spec" = {
+#       "template" = {
+#         "metadata" = {
+#           "annotations" = {
+#             "cluster-autoscaler.kubernetes.io/safe-to-evict" = "true"
+#           }
+#         }
+#         "spec" = {
+#           "dockerEnabled" = "${var.docker_enabled}"
+#           "dockerdWithinRunnerContainer" = "${var.docker_enabled_in_runner_container}"
+#           "ephemeral" = "${var.ephemeral}"
+#           "image" = "${var.container_registry}/${var.container_image}:${var.container_tag}"
+#           "imagePullPolicy" = "IfNotPresent"
+#           "organization" = "${var.github_org}"
+#           "tolerations" = [
+#             {
+#               "effect" = "NoExecute"
+#               "key" = "node.kubernetes.io/unreachable"
+#               "operator" = "Exists"
+#               "tolerationSeconds" = 10
+#             },
+#           ]
+#           "workDir" = "/home/runner/work"
+#         }
+#       }
+#     }
+#   }
+# }
 
 # resource "kubernetes_manifest" "horizontalrunnerautoscaler_actions_runner_system_runner_deployment_autoscaler" {
 #   manifest = {
